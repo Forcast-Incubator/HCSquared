e9f46e2db2fe801d0b0793497a247105f6063e22
Oscar <oscarlouw@gmail.com>
2018-05-14 11:42:05 +0200

    Started work on material instancing

---
+++ Assets/PlantColourChanger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlantColourChanger : MonoBehaviour {
+
+
+    public Color plantColor;
+    public Material plantMaterial;
+	
+	void Update () {
+        plantMaterial.SetColor("_Color", plantColor);
+	}
+}
