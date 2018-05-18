b4307ab32edcfead9c78de6b91ac0035cf3168e9
Oscar <oscarlouw@gmail.com>
2018-05-18 22:45:03 +0200

    Sending main light position as float from 0 to 1 each frame

--- Assets/MidiBridge/TriggerNote.cs
+++ Assets/MidiBridge/TriggerNote.cs
@@ -7,11 +7,10 @@ public class TriggerNote : MonoBehaviour {
     public MidiChannel channel = MidiChannel.Ch1;
 
     public int noteNumber = 48;
-    public bool playingNote = false;
     public float velocity = 0.9f;
-    public float duration = 0.1f;
 
-    private float timer = 0;
+    public bool playingNote = false;
+    private bool playingLastFrame = false;
 
     void Start()
     {
@@ -20,21 +19,15 @@ public class TriggerNote : MonoBehaviour {
     }
     void Update()
     {
-        if(playingNote)
+        if((playingNote == true) && (playingLastFrame == false))
         {
-            if(timer == 0)
-            {
-                MidiOut.SendNoteOn(channel, noteNumber, velocity);
-                timer += Time.deltaTime;
-            } else if(timer < duration)
-            {
-                timer += Time.deltaTime;
-            } else
-            {
-                MidiOut.SendNoteOff(channel, noteNumber);
-                timer = 0;
-                playingNote = false;
-            }
+            MidiOut.SendNoteOn(channel, noteNumber, velocity);
+            playingLastFrame = true;
+            
+        } else if ((playingNote == false) && (playingLastFrame == true))
+        {
+            MidiOut.SendNoteOff(channel, noteNumber);
+            playingLastFrame = false;
         }
     }
 }
